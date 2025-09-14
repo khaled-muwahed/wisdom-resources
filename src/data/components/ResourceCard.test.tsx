e3a0e8ef90861d9ece83848e820cf2a33c111218
khaled-muwahed <muwahedkhaled@gmail.com>
2025-09-14 21:36:23 +0100

    feat: add ResourceCard

--- src/data/components/ResourceCard.test.tsx
+++ src/data/components/ResourceCard.test.tsx
@@ -1,20 +1,19 @@
-import { render , screen } from "@testing-library/react";
-import { expect, it } from "vitest";
-import { describe } from "vitest";
+import { render, screen } from "@testing-library/react";
+import { describe, expect, it } from "vitest";
 import { resources } from "../resources.mock";
+import { ResourceCard } from "./ResourceCard";
 
-
-describe('<ResourceCard />', () => {
-  it('should render title, description,tags, and category', () => {
+describe("<ResourceCard />", () => {
+  it("should render title, description,tags, and category", () => {
     const resource = resources[0];
     render(<ResourceCard resource={resource} />);
 
     expect(screen.getByText(resource.title)).toBeInTheDocument();
 
-    resource.tags.forEach(tag => {
+    resource.tags.forEach((tag) => {
       expect(screen.getByText(tag)).toBeInTheDocument();
     });
     expect(screen.getByText(resource.description)).toBeInTheDocument();
-    expect(screen.getByText(resource.category)).toBeInTheDocument();
-
+    expect(screen.getByText(/Podcasts/i)).toBeInTheDocument();
   });
+});
